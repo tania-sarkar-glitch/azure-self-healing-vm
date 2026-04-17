634534551287fc191601e7618d236a3ae08efa35
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-04-17 12:14:54 +0530

    Create trigger.sh

---
+++ scripts/trigger.sh
@@ -0,0 +1,3 @@
+curl -X POST https://prod-117.westus.logic.azure.com:443/workflows/94da3f6d9db143dba29944d57e4d4906/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=FG-xSnZcw_ja_pEuISRKplPxhROTiMirhw4xb1AhbDA \
+  -H "Content-Type: application/json" \
+  -d '{}'
