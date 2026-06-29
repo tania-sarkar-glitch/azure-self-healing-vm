13c76aa8af4aba135739c17a59927a42e352318d
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-06-30 03:24:31 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,19 @@
+# ======================================
+# Outputs
+# ======================================
 
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.vnet.name
+}
