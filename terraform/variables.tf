f9d91e929233f7ecbba07b9f8badec2b9f2240a9
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-06-30 03:04:26 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,48 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
 
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "vm_name" {
+  description = "Name of the Virtual Machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Administrator username for the VM"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Azure Virtual Machine size"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Virtual Network Address Space"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  description = "Subnet Address Prefix"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
