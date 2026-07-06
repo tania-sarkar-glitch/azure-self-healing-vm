e3efa8c17a2bc6385e812784524fdca98ab43957
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-07-06 20:51:54 +0000

    Update files

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -2,7 +2,7 @@ module "key_vault" {
 
   source = "./modules/key-vault"
 
-  key_vault_name     = var.key_vault_name
+  key_vault_name      = var.key_vault_name
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
 
