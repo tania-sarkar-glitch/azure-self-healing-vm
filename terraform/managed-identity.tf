b435b80673993edf1985b8e5eb1f39f7e1de7bc0
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-07-06 21:01:03 +0000

    Update files

--- terraform/managed-identity.tf
+++ terraform/managed-identity.tf
@@ -2,7 +2,7 @@ module "managed_identity" {
 
   source = "./modules/managed-identity"
 
-  identity_name      = var.identity_name
+  identity_name       = var.identity_name
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
 
