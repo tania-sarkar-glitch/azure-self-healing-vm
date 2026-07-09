63684b6cca77fa910e0d6744840d214fb63e710c
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-07-09 23:20:27 +0000

    updating new files

--- terraform/landing-zone/locals.tf
+++ terraform/landing-zone/locals.tf
@@ -1,10 +1,10 @@
 locals {
   common_tags = {
-    Environment = var.environment
-    Project     = "Enterprise Landing Zone"
-    Owner       = "Tania Sarkar"
-    ManagedBy   = "Terraform"
-    Repository  = "azure-self-healing-vm"
+    Environment  = var.environment
+    Project      = "Enterprise Landing Zone"
+    Owner        = "Tania Sarkar"
+    ManagedBy    = "Terraform"
+    Repository   = "azure-self-healing-vm"
     Architecture = "Hub-Spoke"
   }
 }
