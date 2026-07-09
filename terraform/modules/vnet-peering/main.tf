d4451c26059c74c78d9332e3b0ca1dea818eeb62
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-07-09 23:30:11 +0000

    update resource group for vnet peering

--- terraform/modules/vnet-peering/main.tf
+++ terraform/modules/vnet-peering/main.tf
@@ -1,23 +1,23 @@
 resource "azurerm_virtual_network_peering" "hub_to_spoke" {
   name                      = var.hub_to_spoke_name
-  resource_group_name = var.resource_group_name
+  resource_group_name       = var.hub_resource_group
   virtual_network_name      = var.hub_vnet_name
   remote_virtual_network_id = var.spoke_vnet_id
 
   allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
+  allow_forwarded_traffic       = true
+  allow_gateway_transit         = false
+  use_remote_gateways           = false
 }
 
 resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   name                      = var.spoke_to_hub_name
-  resource_group_name = var.resource_group_name
+  resource_group_name       = var.spoke_resource_group
   virtual_network_name      = var.spoke_vnet_name
   remote_virtual_network_id = var.hub_vnet_id
 
   allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
+  allow_forwarded_traffic       = true
+  allow_gateway_transit         = false
+  use_remote_gateways           = false
 }
