b3896e0abcdb13ea6a075ad34dbe067f07c5d423
tania-sarkar-glitch <taniagungunsarkar@gmail.com>
2026-09-09 01:01:30 +0000

    update changes

--- terraform/modules/azure-monitor-agent/main.tf
+++ terraform/modules/azure-monitor-agent/main.tf
@@ -59,3 +59,16 @@ resource "azurerm_monitor_data_collection_rule_association" "vm" {
   data_collection_rule_id = azurerm_monitor_data_collection_rule.self_healing.id
 
 }
+
+resource "azurerm_virtual_machine_extension" "ama" {
+
+  name                 = "AzureMonitorLinuxAgent"
+  virtual_machine_id   = var.vm_id
+  publisher            = "Microsoft.Azure.Monitor"
+  type                 = "AzureMonitorLinuxAgent"
+  type_handler_version = "1.0"
+
+  auto_upgrade_minor_version = true
+
+  tags = var.tags
+}
